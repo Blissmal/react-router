76a70f53dae90d920ed3b10fa6b599bc12ff732c
Blissmal <malutibethuel@gmail.com>
2024-06-14 18:18:19 +0300

    UPDATED

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ProfilesPage from './ProfilesPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <>404 NOT FOUND</>
   },
+  {
+    path: '/profiles',
+    element: <ProfilesPage />
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
