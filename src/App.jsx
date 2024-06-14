ae6605e89ba992a040ce5c8357716c0caf6d92be
Blissmal <malutibethuel@gmail.com>
2024-06-14 19:12:23 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <h1>Home page</h1>
+      <Link to={'/profiles'}>Profiles</Link>
     </div>
   )
 }
