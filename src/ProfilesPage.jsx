070d8efe75bb074de4df7cc013f9da6cb84944a3
Blissmal <malutibethuel@gmail.com>
2024-06-14 18:11:11 +0300

    initial commit

---
+++ src/ProfilesPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfilesPage = () => {
+
+    const profiles = [1, 2, 3, 4, 5]
+
+    return (
+        <div>
+            <h1>Profiles page</h1>
+        </div>
+    )
+}
+
+export default ProfilesPage
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const router = createBrowserRouter([])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
