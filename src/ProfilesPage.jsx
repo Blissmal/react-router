eaba7b90e77bc1e0ce8110154badee23aac105d6
Blissmal <malutibethuel@gmail.com>
2024-06-14 18:41:06 +0300

    updated

--- src/ProfilesPage.jsx
+++ src/ProfilesPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.css"
 
 const ProfilesPage = () => {
 
     const profiles = [1, 2, 3, 4, 5]
 
     return (
-        <div>
+        <div className='p-5'>
             {profiles.map((profile) => (
-                <Link key={profile} to={`/profiles/${profile}`}>
+                <Link className='row' key={profile} to={`/profiles/${profile}`}>
                     Profile: {profile}
                 </Link>
             ))}
