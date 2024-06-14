0b0ff78a75392475fb4579ea9a5f9e3386c9815b
Blissmal <malutibethuel@gmail.com>
2024-06-14 18:23:38 +0300

    added features

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -5,6 +5,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>404 NOT FOUND</h1>
+      {/* Links are preffered to a tags since they perform client side routing */}
       <Link to={'/'}>Home</Link>
     </div>
   )
